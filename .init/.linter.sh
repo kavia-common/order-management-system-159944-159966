42366a2f7572cfa1bd5cdcb04745676b0120e04e
Kavia AI <production@kavia.ai>
2025-08-16 19:34:10 +0000

    CheckPoint - cg7cfa5732

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/order-management-system-159944-159966/store_manager_frontend
+flutter analyze
+
---
+++ store_manager_frontend/android/app/src/main/kotlin/com/example/store_manager_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.store_manager_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
